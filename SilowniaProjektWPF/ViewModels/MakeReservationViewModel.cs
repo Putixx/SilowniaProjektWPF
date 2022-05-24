8af2c29d0c6be78c6d2ab8395f5c4bc7452856af
Putixx <80105950+Putixx@users.noreply.github.com>
2022-05-24 22:32:15 +0200

    ViewModels

---
+++ SilowniaProjektWPF/ViewModels/MakeReservationViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SilowniaProjektWPF.ViewModels
+{
+    public class MakeReservationViewModel : ViewModelBase
+    {
+        private string _passNumber;
+        public string PassNumber
+        {
+            get => _passNumber;
+            set
+            {
+                _passNumber = value;
+                OnPropertyChanged(nameof(PassNumber));
+            }
+        }
+
+        private string _instructorIndex;
+        public string InstructorIndex
+        {
+            get => _instructorIndex;
+            set
+            {
+                _instructorIndex = value;
+                OnPropertyChanged(nameof(InstructorIndex));
+            }
+        }
+
+        private DateTime _startDate;
+        public DateTime StartDate
+        {
+            get => _startDate;
+            set
+            {
+                _startDate = value;
+                OnPropertyChanged(nameof(StartDate));
+            }
+        }
+
+        private DateTime _endDate;
+        public DateTime EndDate
+        {
+            get => _endDate;
+            set
+            {
+                _endDate = value;
+                OnPropertyChanged(nameof(EndDate));
+            }
+        }
+
+        public ICommand SubmitCommand { get; }
+        public ICommand CencelCommand { get; }
+
+        public MakeReservationViewModel() { }
+    }
+}
---
+++ SilowniaProjektWPF/ViewModels/ReservationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using SilowniaProjektWPF.DAL.Models;
+using System;
+
+namespace SilowniaProjektWPF.ViewModels
+{
+    public class ReservationViewModel : ViewModelBase
+    {
+        private readonly Reservation _reservation;
+
+        public string PassNumber => _reservation.PassNumber;
+        public string InstructorIndex => _reservation.InstructorIndex;
+        public DateTime StartDate => _reservation.StartDate;
+        public DateTime EndDate => _reservation.EndDate;
+
+        public ReservationViewModel(Reservation reservation)
+        {
+            _reservation = reservation;
+        }
+    }
+}
